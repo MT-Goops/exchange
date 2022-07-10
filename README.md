90fad2f783e461be3bb19a4bce67326bab0edb9b
MT-Goops <101109971+MT-Goops@users.noreply.github.com>
2022-07-10 17:48:15 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Exchange
+
+This mod adds a mechanism to inject currency into a server economy, as an alternative to the universal-basic-income feature of the currency mod. It is an interesting idea of jonadab (https://forum.minetest.net/viewtopic.php?f=11&t=4345&p=398541#p398541).
+
+I recommend to deactivate income mechanism (in currency) and interest mechanism (in atm, if loaded).
+
+The mod adds a ``Trade counter'' node that allows to obtain minegeld from glooptest mod "rare" metals.
+
+#### Where to find me ?  
+
+I usually play on the Minerland server, and if not online, there is a mailbox at my home (in front of the travelnet cabine)
+
+> Amelaye in Minerland \<188.165.239.63:30000\>  
+> More infos : http://minetest.amelieonline.net  
---
+++ init.lua
@@ -0,0 +1,110 @@
+
+-- Glooptest "rare" metals
+
+local metals = {
+  akalin = "glooptest:akalin_ingot",
+  alatro = "glooptest:alatro_ingot",
+  arol = "glooptest:arol_ingot",
+  talinite = "glooptest:talinite_ingot"
+}
+
+for m,s in pairs(metals) do
+  local def = minetest.registered_craftitems[s]
+  local groups = table.copy(def.groups)
+  groups.glooptest_ingot = 1
+  minetest.override_item(s, { groups=groups })
+end
+
+-- Trade
+
+local banknote = "currency:minegeld_5"
+
+local Formspec = {
+  "size[8,7.25]",
+  "label[2.5,0.15; _National Central Stock_ ]",
+  "label[2.8,0.5; we buy rare metals ]",
+  "button_exit[7,1;1,2.5;Quit;Quit]",
+  "field[0.25,1;1,2.5;quantity;;1]",
+  "item_image[2,1;1,1;".. metals.akalin .."]",
+  "item_image[3,1;1,1;".. metals.alatro .."]",
+  "item_image[4,1;1,1;".. metals.arol .."]",
+  "item_image[5,1;1,1;".. metals.talinite .."]",
+  "item_image_button[2,2;1,1;".. banknote ..";o1;]",
+  "item_image_button[3,2;1,1;".. banknote ..";o2;]",
+  "item_image_button[4,2;1,1;".. banknote ..";o3;]",
+  "item_image_button[5,2;1,1;".. banknote ..";o4;]",
+  "list[current_player;main;0,3;8,1;]",
+  "list[current_player;main;0,4.25;8,3;8]",
+  "listring[]"
+}
+
+local function trade(player, stackin, stackout)
+    local playername = player:get_player_name()
+    local inv=player:get_inventory()
+    if inv:contains_item("main", stackin) then
+      inv:remove_item("main", stackin)
+      if inv:room_for_item("main", stackout) then
+        inv:add_item("main", stackout)
+      else
+        inv:add_item("main", stackin)
+        minetest.chat_send_player(playername, "Not enough room in your inventory")
+      end
+    else
+      minetest.chat_send_player(playername, "Not enough metal in your inventory")
+    end
+end
+
+
+-- Counter
+
+
+minetest.register_node("goops_exchange:counter", {
+  description = "Trade counter",
+  tiles = {
+    "goops_exchange_side.png",
+    "goops_exchange_side.png",
+    "goops_exchange_side.png",
+    "goops_exchange_side.png",
+    "goops_exchange_side.png",
+    "goops_exchange_front.png"
+  },
+  paramtype2 = "facedir",
+  groups = {cracky=2},
+  after_place_node = function(pos, placer)
+    minetest.get_meta(pos):set_string("formspec", table.concat(Formspec, ""))
+  end,
+  on_receive_fields = function(pos, formname, fields, player)
+    local playername = player:get_player_name()
+    if fields.o1 then
+      local stackin = metals.akalin.." "..fields.quantity
+      local stackout = banknote.." "..fields.quantity
+      trade(player,stackin,stackout)
+    elseif fields.o2 then
+      local stackin = metals.alatro.." "..fields.quantity
+      local stackout = banknote.." "..fields.quantity
+      trade(player,stackin,stackout)
+    elseif fields.o3 then
+      local stackin = metals.arol.." "..fields.quantity
+      local stackout = banknote.." "..fields.quantity
+      trade(player,stackin,stackout)
+    elseif fields.o4 then
+      local stackin = metals.talinite.." "..fields.quantity
+      local stackout = banknote.." "..fields.quantity
+      trade(player,stackin,stackout)
+    end
+    if fields.quit then
+      minetest.chat_send_player(playername, "Thanks for trading with the National Central Stock")
+      minetest.chat_send_player(playername, "See you soon !")
+      return
+    end
+  end
+})
+
+minetest.register_craft({
+	output = "goops_exchange:counter",
+	recipe = {
+		{"default:gold_ingot", "default:mese_crystal", "default:gold_ingot"},
+		{"default:gold_ingot", "group:glooptest_ingot", "default:gold_ingot"},
+		{"default:gold_ingot", "default:mese_crystal", "default:gold_ingot"}
+	}
+})
